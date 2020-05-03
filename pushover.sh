c07c6187b2cbeed2910fc8896f6a7b840797d428
Jefta Dirksen <jeftadirksen@gmail.com>
2020-05-03 15:48:52 +0200

    Pushover logging

--- pushover.sh
+++ pushover.sh
@@ -1,6 +1,8 @@
+echo "$1" >> pushover.log
 curl -s \
   --form-string "token=APP_TOKEN" \
   --form-string "user=USER_KEY" \
   --form-string "message=$1" \
-  https://api.pushover.net/1/messages.json
-echo
+  https://api.pushover.net/1/messages.json \
+  >> pushover.log
+echo >> pushover.log
