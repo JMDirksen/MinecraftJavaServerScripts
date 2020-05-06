56a77fd93655de4c2c044212792ee119c3c41373
Jefta Dirksen <jeftadirksen@gmail.com>
2020-05-06 12:45:29 +0200

    logmon ignore Fetching packet/addPacket

--- logmon.sh
+++ logmon.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"
 
 [ $# -eq 0 ] && tail -F -n1 logs/latest.log | xargs -n1 -d '\n' ./logmon.sh
-[[ "$1" =~ "[Server thread/WARN]" ]] && ./pushover.sh "$1"
+[[ "$1" =~ "[Server thread/WARN]" ]] && [[ ! "$1" =~ "]: Fetching" ]] && ./pushover.sh "$1"
