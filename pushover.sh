78acb8c1b2c5f20b6db5446becb044d0190261cf
Jefta Dirksen <jeftadirksen@gmail.com>
2020-05-08 07:48:40 +0200

    Pushover conf file

--- pushover.sh
+++ pushover.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+[ ! -f pushover.conf ] && { echo Missing pushover.conf; exit 1; }
+. pushover.conf
+
 echo "$1" >> pushover.log
 curl -s \
-  --form-string "token=APP_TOKEN" \
-  --form-string "user=USER_KEY" \
+  --form-string "user=$user" \
+  --form-string "token=$token" \
   --form-string "message=$1" \
   https://api.pushover.net/1/messages.json \
   >> pushover.log
