ec1b386f0e238f797042d8a2d1029bf8c0a99cfa
Jefta Dirksen <jeftadirksen@gmail.com>
2020-05-08 07:57:39 +0200

    Fix: No FATAL/ERROR type messages

--- logmon.sh
+++ logmon.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"
 
 [ $# -eq 0 ] && tail -F -n1 logs/latest.log | xargs -n1 -d '\n' ./logmon.sh
-[[ "$1" =~ "[Server thread/WARN]" ]] && [[ ! "$1" =~ "]: Fetching" ]] && [[ ! "$1" =~ "]: Mismatch" ]] && ./pushover.sh "$1"
+[[ ! "$1" =~ "/INFO]:" ]] && [[ ! "$1" =~ "]: Fetching" ]] && [[ ! "$1" =~ "]: Mismatch" ]] && ./pushover.sh "$1"
